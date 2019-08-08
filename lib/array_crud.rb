c5a43442ef87f6b7c301190b62af2de1433fe3fb
Tanya Bihag <tbihag@gmail.com>
2019-08-08 02:52:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,8 @@ def retrieve_element_from_index(array, index_number) ["Sun", "Moon", "Sky", "Rai
   
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array(array) ["Sun", "Moon", "Sky", "Rain"]
+  retrieve_first_element_from_array[0]
   
 end
 
