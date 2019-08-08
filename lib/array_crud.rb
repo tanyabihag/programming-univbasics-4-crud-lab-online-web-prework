05646944b9b942d0e1ba9cc5033d60fdf40a74b7
Tanya Bihag <tbihag@gmail.com>
2019-08-08 02:51:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,8 @@ def remove_element_from_start_of_array(array) ["Sun", "Moon", "Sky", "Rain"]
   
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(array, index_number) ["Sun", "Moon", "Sky", "Rain"]
+  retrieve_element_from_index[1]
   
 end
 
